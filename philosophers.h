932e5917d85806c7558c4fed70b8ea4ecdcb03b0
makpolatt <emreakpolat3@gmail.com>
2025-06-24 15:18:29 +0300

    philo başlangıç / struct oluşturuldu

---
+++ philosophers.h
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosophers.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: makpolat <makpolat@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/24 15:08:34 by makpolat          #+#    #+#             */
+/*   Updated: 2025/06/24 15:13:05 by makpolat         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#ifndef PHILOSOPHERS_H
+# define PHILOSOPHERS_H
+
+
+
+typdef struct s_philo{
+
+    int number_of_philo;
+    int philo_eat_time;
+    int philo_dead_time;
+    int philo_think_time;
+    int philo_sleep_time;
+}
+
+
+
+
+
+
+
+
+
+#endif
